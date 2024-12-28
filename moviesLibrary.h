7f5b4a9da2a2d5506d656afbba66edb5dcb054d8
Freddy Fernandez <146951144+fafernandez24@users.noreply.github.com>
2024-12-28 03:05:24 -0400

    Update and rename movies_library.h to moviesLibrary.h

--- moviesLibrary.h
+++ moviesLibrary.h
@@ -17,9 +17,9 @@ struct Movie{
     int movie_premiere;
     string movie_type;
     int movie_time;
+    int movie_id;
 
     Movie *next_movie;
-    Movie *prev_movie;
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,13 +56,26 @@ bool checkIntDataTime(string word){
     return false;
 }
 
+bool checkIntMovieId(string word){
+
+    if (word.empty()) return false;
+
+    for (sizeType n = 0; n < word.length(); ++n){
+        if (word[n] < '0' || word[n] > '9') return false;
+    }
+
+    if (stoi(word) >= 0 && stoi(word) <= 9999999) return true;
+
+    return false;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 // FUNCIONES & PROCEDIMIENTOS
 
 /* Funcion que busca retornar un puntero que apunte a un elemento de tipo pelicula */
 
-Movie *createMovie(string name, int premiere, string type, int time){
+Movie *createMovie(string name, int premiere, string type, int time, int id){
 
   Movie *new_movie = new Movie;
 
@@ -70,11 +83,22 @@ Movie *createMovie(string name, int premiere, string type, int time){
   new_movie -> movie_premiere = premiere;
   new_movie -> movie_type = type;
   new_movie -> movie_time = time;
+  new_movie -> movie_id = id;
 
   new_movie -> next_movie = NULL;
-  new_movie -> prev_movie = NULL;
 
   return new_movie;
+}
+
+Movie *getMovieNode(Movie *movies_head, string movie_name){
+  if (movies_head){
+    Movie *aux = movies_head;
+    while (aux){
+      if (aux -> movie_name == movie_name) return aux;
+      aux = aux->next_movie;
+    }
+  }
+  return movies_head;
 }
 
 /* FUNCIONES PARA OBTENER LOS DATOS DE LAS PELICULAS */
@@ -84,7 +108,7 @@ string getMovieName(){
 
   string movie_name;
   cin.ignore();
-  cout << "\tNombre: ";
+  cout << "\tNombre de pelicula: ";
   getline(cin, movie_name);
   return movie_name;
 } 
@@ -138,11 +162,29 @@ int getMovieTime(){
   return stoi(movie_time);
 }
 
+int getMovieId(){
+
+  string movie_id;
+  bool check_num = false;
+  
+  while (check_num == false){
+    cout << "\tID: ";
+    cin >> movie_id;
+
+    check_num = checkIntMovieId(movie_id);
+    if (check_num == false){
+      cout << "ERROR. Ingresaste un valor invalido!\n";
+      cout << "AVISO: Igresar un entero entre 10000 y 99999!\n";
+    }
+  }
+  return stoi(movie_id);
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 /* Procedimiento para mostras las peliculas en pantalla */
 
-void printMovie(Movie *movie_head){
+void printMovieList(Movie *movie_head){
 
   Movie *aux = movie_head;
   int movie_num = 1;
@@ -160,6 +202,7 @@ void printMovie(Movie *movie_head){
       cout << "\tMovie premiere: " << aux->movie_premiere << endl;
       cout << "\tMovie type: " << aux->movie_type << endl;
       cout << "\tMovie time: " << aux->movie_time << " min" << endl;
+      cout << "\tMovie ID: " << aux -> movie_id << endl;
       cout << "========================================\n";
 
       aux = aux->next_movie;
@@ -179,9 +222,9 @@ void printMovie(Movie *movie_head){
 
 /* Procedimiento para agregar peliculas */
 
-void addMovie(Movie **movie_head, string name, int premiere, string type, int time){
+void addMovie(Movie **movie_head, string name, int premiere, string type, int time, int id){
 
-    Movie *new_movie = createMovie(name,premiere, type, time);
+    Movie *new_movie = createMovie(name, premiere, type, time, id);
 
     if (!*movie_head){
       *movie_head = new_movie;
@@ -189,7 +232,6 @@ void addMovie(Movie **movie_head, string name, int premiere, string type, int ti
     else{
       new_movie -> next_movie = *movie_head;
       *movie_head = new_movie;   
-      new_movie -> next_movie -> prev_movie = *movie_head;
     }
 }
 
@@ -221,31 +263,6 @@ void deleteMovie(Movie **movie_head, string name){
   }
 }
 
-void searchMovie(Movie *movie_head, string name){
-   
-   if (movie_head){
-
-      Movie *aux = movie_head;
-      int movie_num = 1;
-
-      while (aux -> movie_name != name){
-        aux = aux -> next_movie;
-        ++movie_num;
-      }
-
-      cout << "              Movie #" << movie_num << "              \n";
-      cout << "========================================\n";
-      cout << "\tMovie name: " << aux->movie_name << endl;
-      cout << "\tMovie premiere: " << aux->movie_premiere << endl;
-      cout << "\tMovie type: " << aux->movie_type << endl;
-      cout << "\tMovie time: " << aux->movie_time << " min" << endl;
-      cout << "========================================\n";
-   }
-   else{
-      cout << "ERROR. Lista de peliculas vacia.\n";
-   }
-}
-
 /* FUNCION QUE RETORNA UN TIPO DE DATO BOOLEANO QUE DETERMINA SI UNA PELICA SE ENCUENTRA O NO EN LA LISTA DE PELICULAS */
 
 bool findMovie(Movie *movie_head, string name){
@@ -269,9 +286,8 @@ void readMoviesFile(Movie **movie_head){
 
   ifstream file;
   string text, movie_name, movie_type;
-  int data_line = 0, movie_premiere, movie_time;
+  int data_line = 0, movie_premiere, movie_time, movie_id;
   Movie *aux = *movie_head;
-
 
   file.open("moviesfile.txt", ios::in);
 
@@ -293,7 +309,6 @@ void readMoviesFile(Movie **movie_head){
 
         if (checkIntDataTime(text) == true){
           movie_time = stoi(text);
-          addMovie(&aux, movie_name, movie_premiere, movie_type, movie_time);
         }
         else{
           cout << "\nERROR. Dato invalidos en el archivo!\n";
@@ -314,10 +329,24 @@ void readMoviesFile(Movie **movie_head){
 
       if (data_line == 1) movie_name = text;
 
+      if (data_line == 5){
+
+        if (checkIntMovieId(text) == true){
+          movie_id = stoi(text);
+          addMovie(&aux, movie_name, movie_premiere, movie_type, movie_time, movie_id);
+        }
+        else{
+          cout << "\nERROR. Dato invalidos en el archivo!\n";
+          cout << "AVISO: No se cargaron los datos del archivo de peliculas!\n\n";
+          file.close();
+        }
+      }
+
       ++data_line;
 
-      if (data_line == 5) data_line = 0;
-  }
+      if (data_line == 6) data_line = 0;
+
+    }
 
   *movie_head = aux;
 
@@ -333,7 +362,7 @@ void readMoviesFile(Movie **movie_head){
 
 /* PROCEDIMIENTO PARA LA ESCRITURA DE ARCHIVOS EN PELICULAS */
 
-void addMovieToFile(string movie_name, int movie_premiere, string movie_type, int movie_time){
+void addMovieToFile(string movie_name, int movie_premiere, string movie_type, int movie_time, int movie_id){
 
     ofstream file;
 
@@ -349,6 +378,7 @@ void addMovieToFile(string movie_name, int movie_premiere, string movie_type, in
     file << endl << movie_premiere;
     file << endl << movie_type;
     file << endl << movie_time;
+    file << endl << movie_id;
 
     file.close();
 }
@@ -376,9 +406,10 @@ void writeMovieFile(Movie *movie_head){
         file << aux -> movie_name << endl;
         file << aux -> movie_premiere << endl;
         file << aux -> movie_type << endl;
+        file << aux -> movie_time << endl;
 
-        if (aux -> next_movie == NULL) file << aux -> movie_time;
-        else file << aux -> movie_time << endl << endl;
+        if (aux -> next_movie == NULL) file << aux -> movie_id;
+        else file << aux -> movie_id << endl << endl;
 
         aux = aux -> next_movie;
       }
@@ -404,13 +435,15 @@ int movieMenu(){
   cout << "(2) Agregar pelicula\n";
   cout << "(3) Eliminar pelicula\n";
   cout << "(4) Buscar pelicula\n";
-  cout << "(0) Salir\n";
+  cout << "(5) Calificaciones\n";
+  cout << "(6) Reviews\n";
+  cout << "(0) Regresar al menu principal\n";
   cout << "========================================" << endl;
   cout << "Ingresar opcion: ";
   cin >> menu;
   cout << "========================================\n";
   
-  if (menu >= "0"  && menu <= "3") return stoi(menu);
+  if (menu >= "0"  && menu <= "6") return stoi(menu);
 
   cout << "========================================\n";
   cout << "ERROR. Ingresaste " << menu << " que es un valor invalido.\n";
@@ -430,41 +463,22 @@ void addMovieMenu(Movie **movie_head){
   int movie_premiere = getMoviePremiere();
   string movie_type = getMovieType();
   int movie_time = getMovieTime();
+  int movie_id = getMovieId();
   cout << "========================================\n";
 
   bool movie_exist = findMovie(*movie_head, movie_name);
 
   if (movie_exist == false){
-    addMovie(&*movie_head, movie_name, movie_premiere, movie_type, movie_time);
-    addMovieToFile(movie_name, movie_premiere, movie_type, movie_time);
-    cout << "La pelicula se agrego correctamente!\n";
+    addMovie(&*movie_head, movie_name, movie_premiere, movie_type, movie_time, movie_id);
+    addMovieToFile(movie_name, movie_premiere, movie_type, movie_time, movie_id);
+    cout << "AVISO: La pelicula se agrego correctamente!\n";
   }
   else cout << "AVISO: La pelicula ya se encuentra agregada!\n";
 }
 
-/* PROCEDIMIENTO DEL MENU PARA BORRAR LA PELICULA */
-
-void deleteMovieMenu(Movie **movie_head){
-
-  cout << "========================================\n";
-  cout << "              DELETE MOVIE              \n";
-  cout << "========================================\n";
-  string movie_name = getMovieName();
-  cout << "========================================\n";
-
-  bool movie_exist = findMovie(*movie_head, movie_name);
-
-  if (movie_exist == true){
-    deleteMovie(&*movie_head, movie_name);
-    writeMovieFile(*movie_head);
-    cout << "La pelicula se borro correctamente!\n";
-  }
-  else cout << "ERROR. No se encontro la pelicula!\n";
-}
-
 /* PROCEDIMIENTO DEL MENU PARA BUSCAR LA PELICULA */
 
-void searchMovieMenu(Movie *movie_head){
+string searchMovieMenu(){
 
   cout << "========================================\n";
   cout << "              SEARCH MOVIE              \n";
@@ -472,10 +486,7 @@ void searchMovieMenu(Movie *movie_head){
   string movie_name = getMovieName();
   cout << "========================================\n";
 
-  bool movie_exist = findMovie(movie_head, movie_name);
-
-  if (movie_exist == true) searchMovie(movie_head, movie_name);
-  else cout << "ERROR. No se encontro la pelicula!\n";
+  return movie_name;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
